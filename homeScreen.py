1a87775c4a1db2c9427b330ef6148fc0f7536c7d
Karthik R <karthikr.ofc@gmail.com>
2023-02-05 17:14:08 +0530

    UI in homescreen

--- homeScreen.py
+++ homeScreen.py
@@ -11,11 +11,28 @@ def homeScreen(uid,username):
     ess()
     uiApp = customtkinter.CTk()
     uiApp.geometry(f"{600}x{500}")
+    uiApp.title("LIveFire Dashboard")
     
     id = str(uid)
     
-    title = "Welcome "+username+" to LiveFire Detection "+id
-    label = customtkinter.CTkLabel( master="", text=title, text_color="#FFFFFF",font=("montserrat",30))
-    label.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
+    uiApp.columnconfigure(1, weight=0)
+    uiApp.rowconfigure(0, weight=1)
+
+    optionsFrame = customtkinter.CTkFrame(master="", fg_color="transparent", border_color="#DC5F00" ,border_width= 2)
+    optionsFrame.configure(width=150)
+    optionsFrame.grid(row =0, column=0, sticky='ns')
+    optionsFrame.columnconfigure(0, weight=1)
+    optionsFrame.rowconfigure(0, weight=1)
+    
+    homeFrame = customtkinter.CTkFrame(master="",border_color="#Defd00", fg_color="transparent")
+    uiApp.grid_columnconfigure(1, weight=1)
+    homeFrame.grid(row=0, column=1, sticky = 'nsew')
+    homeFrame.columnconfigure(1, weight=1)
+    
+    
+    title = "Welcome "+username+" to LiveFire Detection "
+    label = customtkinter.CTkLabel( master="", text=title, text_color="#FFFFFF",font=("montserrat",20), padx=15, pady=30)
+    label.grid(row=0, column=1, sticky = 'nw')
+    # label.place(relx=0.6, rely=0.1, anchor=tkinter.CENTER)
     
     uiApp.mainloop()
